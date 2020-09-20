efbc5faeea1150906b790dc3dd92bd1f13612f0f
Hadis-Hashemi <hashemi1987@yahoo.com>
2020-09-20 15:56:32 -0500

    cleaning the code

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -1,5 +1,5 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
 
 
 CREATE TABLE "DEPARTMENTS" (
